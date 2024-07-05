ac82f4f18ff38e41421fe3be72623f183022e5ef
Yago Mendes <119672907+DevMendes21@users.noreply.github.com>
2024-07-05 14:55:33 -0300

    FEAT
    TREINAMENTO COMO FAZER UM SERVER EM TS

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+// npm i express
+//npm i --save-dev @types/express
+//Comando para rodar: npm run lint ou npm run lint:fix (para consertar e indicar erros do codigo)
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+// Indicando que a API vai usar o JSON
+app.use(express.json());
+
+//Req = > Requset (Requisição)
+//Res = > Response (Resposta)
+app.use('/', (req: Request, res: Response) => {
+  return res.status(200).json({
+    msg: 'Estamos online',
+    live: true,
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Estamos online');
+  console.log('Acesso http://localhost:5000');
+});
+//npm install prettier eslint-config-prettier eslint-plugin-prettier --save-dev
